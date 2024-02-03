e131115b110b8e773a24951c87a67782e3f2b3ae
Harsh Kumar <140370637+sgnhyperion@users.noreply.github.com>
2024-02-04 02:35:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let postbutton = document.querySelector("#post-button");
 let board = document.querySelector(".right-container");
 let text = document.querySelector("#text-area");
+let wordcount = document.querySelector("#word-count");
 
 postbutton.addEventListener("click", function(){
     let post = document.createElement("div");
+    post.style.textAlign = "center";
     post.id = "created-post";
     post.innerHTML = text.value;
+    let count = text.value.length;
+    wordcount.innerHTML = count + "/100";
     text.value = "";
     let react = document.createElement("div");
     react.id = "react";
@@ -27,25 +31,34 @@ postbutton.addEventListener("click", function(){
     like.addEventListener("click", function(){
         like.src = "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/025/original/state_clicked.png?1706888455";
     });
+
     del.addEventListener("click", function(){
-        post.remove();
+        var result = confirm("Want to delete?");
+        if (result) {
+            post.remove();
+        }
     });
+
     comment.addEventListener("click", function(){
-        let commentbox = document.createElement("input");
-        commentbox.id = "comment-box";
-        commentbox.type = "text";
-        commentbox.placeholder = "Comment here";
-        post.appendChild(commentbox);
-        let commentbutton = document.createElement("button");
-        commentbutton.id = "comment-button";
-        commentbutton.innerHTML = "Comment";
-        post.appendChild(commentbutton);
-        commentbutton.addEventListener("click", function(){
-            let comment = document.createElement("div");
-            comment.id = "comment";
-            comment.innerHTML = commentbox.value;
-            commentbox.value = "";
-            post.appendChild(comment);
+        let commentbox = document.createElement("div");
+        let textbox = document.createElement("textarea");
+        textbox.id = "text-box";
+        textbox.placeholder = "Write a comment...";
+        let c_btn = document.createElement("button");
+        c_btn.id = "c-btn";
+        c_btn.innerText = "Comment";
+        commentbox.appendChild(textbox);
+        commentbox.appendChild(c_btn);
+        board.appendChild(commentbox);
+
+        c_btn.addEventListener("click",function(){
+            let realcmt = document.createElement("div");
+            realcmt.id = "real-cmt";
+            realcmt.innerHTML = textbox.value;
+            realcmt.appendChild(react);
+            console.log(commentbox.innerHTML);
+            commentbox.remove();
+            board.appendChild(realcmt);
         });
     });
 });
